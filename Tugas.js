b09b059181aa865935103e3252c7ce9e7ce8db6e
rahmanrizaluddin <114675284+rahmanrizaluddin@users.noreply.github.com>
2023-09-26 22:15:42 -0700

    Add files via upload

---
+++ Tugas.js
@@ -0,0 +1,21 @@
+const Nama = "Rahman Rizaluddin"
+const NIM = 20220040295
+
+// aritmatika Pada javaScript
+
+const angka1 = 20
+const angka2 = 10
+
+const Tambah = angka1 + angka2 ;
+const Kurang = angka1 + angka2 ;
+const Kali = angka1 * angka2 ;
+const Bagi = angka1 / angka2 ;
+
+// input variabel
+
+console.log( 'Nama saya' + Nama )
+console.log( 'NIM Saya' + NIM )
+console.log( "Pertambahan" + Tambah )
+console.log( "Pengurangan" + Kurang )
+console.log( "Pengurangan" + Kali )
+console.log( "Pembagiab" + Bagi )
